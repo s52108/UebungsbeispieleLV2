175f7174338f233981cc30991a1b105370ca9336
postl <s52108@edu.campus02.at>
2022-10-02 20:47:46 +0200

    Added TurmDemo

---
+++ TurmDemo.java
@@ -0,0 +1,16 @@
+public class TurmDemo {
+    public static void main(String[] args) {
+        int a = 2;
+        int count = 2;
+
+        //Bedingung für while definieren
+        while (count <= 10) {
+            //Schleifenstart
+            System.out.println(a + " * " + count + " = " + (a * count));
+            count++;
+
+        }
+
+    }
+
+}
